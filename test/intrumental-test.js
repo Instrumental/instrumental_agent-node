fada981c12b21f521737c99cd4b49e72d3adfaa0
Grant McNaught <gmcnaught@salesforce.com>
2016-03-18 15:30:06 -0400

    updated tests to try and trigger queue event, added notice test, fixed version hardcode in tests

--- test/intrumental-test.js
+++ test/intrumental-test.js
@@ -3,6 +3,7 @@
 var Instrumental = require('../lib/instrumental');
 var mitm = require('mitm');
 require('should');
+const VERSION = require('../package.json').version;
 
 describe('Instrumental', function() {
   beforeEach(function() { this.mitm = mitm(); });
@@ -10,7 +11,7 @@ describe('Instrumental', function() {
 
   it('should send gauge calls correctly', function(done) {
     var expectedData = [
-      ['hello version node/instrumental_agent/0.1.0\nauthenticate test\n', 'ok\nok\n'],
+      ['hello version node/instrumental_agent/'+VERSION+'\nauthenticate test\n', 'ok\nok\n'],
       ['gauge test.metric 5 1455477257 1\n']];
     var index = 0;
 
@@ -39,7 +40,7 @@ describe('Instrumental', function() {
 
   it('should send increment calls correctly', function(done) {
     var expectedData = [
-      ['hello version node/instrumental_agent/0.1.0\nauthenticate test\n', 'ok\nok\n'],
+      ['hello version node/instrumental_agent/'+VERSION+'\nauthenticate test\n', 'ok\nok\n'],
       ['increment test.metric 5 1455477257 1\n']];
     var index = 0;
 
@@ -57,14 +58,52 @@ describe('Instrumental', function() {
           expectedData.push(['increment test.metric2 1 ' +
             Math.round(Date.now()/1000) + ' 1\n']);
           I.increment('test.metric2');
-        } else if(index === 3) {
-          done();
+	  }
+	  else if(index === 3){
+		  expectedData.push(['increment test.metric3 1 ' +
+			Math.round(Date.now()/1000) + ' 1\n']);
+		  //this should trigger addition to the socket queue.
+		  I.increment('test.metric3');
+		  I.increment('test.metric4');
+
+	  }
+	  else if(index === 4) {
+		  expectedData.push(['increment test.metric4 1 ' +
+		  Math.round(Date.now()/1000) + ' 1\n']);
         }
+	  else if(index === 5){
+		  done();
+	  }
       });
     });
 
     I.increment('test.metric', 5, new Date(1455477257165));
     I.increment('discard.cause.zero', 0);
+  });
+
+  it('should send notice calls correctly', function(done) {
+    var expectedData = [
+      ['hello version node/instrumental_agent/'+VERSION+'\nauthenticate test\n', 'ok\nok\n'],
+      ['notice 1455477257 0 test is good\n']];
+    var index = 0;
+
+    var I = new Instrumental();
+    I.configure({apiKey: 'test', enabled: true});
+
+    this.mitm.on('connection', (socket) => {
+      socket.on('data', (data) => {
+        data.toString().should.equal(expectedData[index][0]);
+        if(expectedData[index][1])
+        { socket.write(expectedData[index][1]); }
+
+        index++;
+	        if(index === 2) {
+	          done();
+		  	}
+      });
+    });
+
+    I.notice(new Date(1455477257165),0,'test is good');
   });
 
 });
